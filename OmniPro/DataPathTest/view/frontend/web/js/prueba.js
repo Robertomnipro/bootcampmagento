a3219e2af149c45a1b97a25e65edf48d72eefb80
RobertoDavidRobles <roberto.robles@omni.pro>
2021-04-12 16:29:30 +0000

    adminhtml frist working

--- OmniPro/DataPathTest/view/frontend/web/js/prueba.js
+++ OmniPro/DataPathTest/view/frontend/web/js/prueba.js
@@ -1,15 +1,50 @@
-console.log('log test');
 requirejs([
     'jquery',
-    'underscore'
-], function ($,_) {
-        return function (config) {
-            console.log(config);
-            let blog = "rest/v1/blog?searchCriteria"
-            $.ajax({url:blogs}).done(function (response) {
-                console.log(response);
+    'underscore',
+    'ko',
+    'uiComponent',
+    'mage/storage',
+    'mage/url',
+], function ($,_,ko,Component, url, storage) {
+
+    return Component.extend({
+        defaults:{
+            blogs:ko.observableArray([]),
+            template: 'OmniPro_DataPathTest/blog',
+            ignotreTmpls: {
+                template: false
+            }
+           
+        },
+        initialize:function () {
+            this._super();
+            console.log(this);
+            console.log(this.variable());
+            this.variable.subscribe(function (value) {
+                console.log(value);
             });
-            
+            let self = this;
+            let blogs = "rest/V1/blogs?searchCriteria"
+            storage.get(blogs)
+            // $.ajax({
+            //     url:blogs
+            // })
+            .done(function (response) {
+                self.blogs(response.items);
+                console.log(self.blogs());
+            });
+            return this;
         }
+    });
+
+        // return function (config) {
+        //     console.log(config{});
+        //     let blog = "rest/v1/blog?searchCriteria"
+        //     $.ajax({url:blogs}).done(function (response) {
+        //         console.log(response);
+        //     });
+            
+        // }
+
         // $('body').addClass('pruebamagento');
 })
--- OmniPro/Prueba/view/web/js/prueba.js
+++ OmniPro/Prueba/view/web/js/prueba.js
@@ -1,14 +1,31 @@
 requirejs([
     'jquery',
-    'underscore'
-], function ($,_) {
-        return function (config) {
-            console.log(config);
-            let blog = "rest/v1/blog?searchCriteria"
-            $.ajax({url:blogs}).done(function (response) {
-                console.log(response);
-            });
-            
+    'underscore',
+    'uiComponent'
+], function ($,_,Component) {
+
+    return Component.extend({
+        defaults:{
+            'variables':'texto 1'
+        },
+        initialize:function () {
+            console.log(this);
+            this._super()
+                let blog = "rest/V1/blogs?searchCriteria"
+                $.ajax({url:blogs}).done(function (response) {
+                    console.log(response);
+                });
         }
+    });
+
+        // return function (config) {
+        //     console.log(config{});
+        //     let blog = "rest/v1/blog?searchCriteria"
+        //     $.ajax({url:blogs}).done(function (response) {
+        //         console.log(response);
+        //     });
+            
+        // }
+
         // $('body').addClass('pruebamagento');
 })
