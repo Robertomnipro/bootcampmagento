f7e94ba460b45f538c23adf10b7be8567075ff65
RobertoDavidRobles <roberto.robles@omni.pro>
2021-04-14 11:24:04 +0000

    testing ko get post blog

--- OmniPro/Prueba/view/frontend/web/js/blogItems.js
+++ OmniPro/Prueba/view/frontend/web/js/blogItems.js
@@ -1,27 +1,39 @@
 console.log('testing');
 define([
     'uiComponent',
+    'mage/storage',
+    'jquery',
     'ko',
-    'jquery'
-], function(Component, ko, $) {
+], function(Component,storage, $,ko) {
     return Component.extend({
         defaults: {
             textoPrueba: "Texto Prueba",
-            variable1: 25
+            variable1: 25,
+            blogItems: []
+            // blogItems: ko.observableArray([]),
         },
         initialize: function() {
             this._super();
+            self=this;
             setTimeout($.proxy(function() {
                 this.textoPrueba("Prueba 2");
                 console.log(this);
-            }, this), 1000)
+            }, this), 1000);
+            let blogs = "/rest/V1/blogs?searchCriteria"
+            storage.get(blogs)     
+            .done(function (response) {
+                console.log(response.items);
+                self.blogItems(response.items);
+                console.log( self.blogItems);
+            });
             return this;
         },
         initObservable: function() {
             this._super()
                 .observe([
                     'variable1',
-                    'textoPrueba'
+                    'textoPrueba',
+                    'blogItems'
                 ]);
 
             return this;
